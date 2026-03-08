5d25d7d27b0c1cc09d479c4ef1e23960a27eec88
Damien Robinson <damien.robinson@pocketpasta.com>
2026-03-09 06:00:35 +1000

    chore: install nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,40 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/a11y',
+    '@nuxt/content',
+    '@nuxt/hints',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@bubblesortt/nuxt-es-toolkit',
+    '@nuxthub/core',
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/seo',
+    '@vite-pwa/nuxt',
+    '@vueuse/nuxt'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
